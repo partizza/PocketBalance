8bbadd9389adce87fb4e535dfe522870d3362840
Anton Gabrylevych <agabrylevych@gmail.com>
2017-02-18 14:14:12 +0200

    Initial commit

---
+++ src/main/java/ua/agwebs/PocketBalanceApplication.java
@@ -0,0 +1,12 @@
+package ua.agwebs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketBalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketBalanceApplication.class, args);
+	}
+}
