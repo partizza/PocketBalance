d5061c54483e6fc4cca710389e0e02f4b6f36fca
Anton Gabrylevych <agabrylevych@gmail.com>
2017-03-07 20:54:51 +0200

    EntryService definition

--- src/main/java/ua/agwebs/root/service/EntryManger.java
+++ src/main/java/ua/agwebs/root/service/EntryManger.java
@@ -1,5 +1,19 @@
 package ua.agwebs.root.service;
 
 
+import ua.agwebs.root.entity.EntryHeader;
+
+import javax.validation.Valid;
+
 public class EntryManger implements EntryService {
+
+    @Override
+    public EntryHeader createEntry(@Valid EntryHeader entryHeader) {
+        return null;
+    }
+
+    @Override
+    public void setStorno(long entryHeaderId, boolean value) {
+
+    }
 }
--- src/main/java/ua/agwebs/root/service/EntryService.java
+++ src/main/java/ua/agwebs/root/service/EntryService.java
@@ -1,5 +1,16 @@
 package ua.agwebs.root.service;
 
 
+import org.springframework.validation.annotation.Validated;
+import ua.agwebs.root.entity.EntryHeader;
+
+import javax.validation.Valid;
+
+@Validated
 public interface EntryService {
+
+    public EntryHeader createEntry(@Valid EntryHeader entryHeader);
+
+    public void setStorno(long entryHeaderId, boolean value);
+
 }
