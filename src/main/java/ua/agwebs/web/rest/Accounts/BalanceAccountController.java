4b24b56e922872eed6aab8c54fa803883621563f
Anton Gabrylevych <agabrylevych@gmail.com>
2017-03-28 22:34:39 +0300

    REST BalanceAccountController impl

--- src/main/java/ua/agwebs/web/rest/Accounts/BalanceAccountController.java
+++ src/main/java/ua/agwebs/web/rest/Accounts/BalanceAccountController.java
@@ -3,10 +3,7 @@ package ua.agwebs.web.rest.Accounts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = {"/data/account"})
@@ -15,20 +12,28 @@ public class BalanceAccountController {
     private BalanceAccountService accountService;
 
     @Autowired
-    public BalanceAccountController(BalanceAccountService accountService){
+    public BalanceAccountController(BalanceAccountService accountService) {
         this.accountService = accountService;
     }
 
     @GetMapping(value = "/{accId}/book/{bookId}")
-    public ResponseEntity<BalanceAccountDTO> getBalanceAccountById(@RequestParam(value = "accId") long accId,
-                                                                   @RequestParam(value = "bookId") long bookId){
-        BalanceAccountDTO accountDTO = accountService.findById(bookId,accId);
+    public ResponseEntity<BalanceAccountDTO> getBalanceAccountById(@PathVariable("accId") long accId,
+                                                                   @PathVariable("bookId") long bookId) {
+        BalanceAccountDTO accountDTO = accountService.findById(bookId, accId);
         ResponseEntity<BalanceAccountDTO> responseEntity;
-        if(accountDTO ==  null){
+        if (accountDTO == null) {
             responseEntity = new ResponseEntity<BalanceAccountDTO>(HttpStatus.NOT_FOUND);
-        }else{
+        } else {
             responseEntity = new ResponseEntity<BalanceAccountDTO>(accountDTO, HttpStatus.OK);
         }
         return responseEntity;
+    }
+
+    @PostMapping(value = "/book/{bookId}")
+    public ResponseEntity<String> createBalanceAccount(@RequestBody BalanceAccountDTO accountDTO,
+                                                       @PathVariable("bookId") long bookId) {
+        accountDTO.setBookId(bookId);
+        accountService.createBalanceAccount(accountDTO);
+        return new ResponseEntity<String>(HttpStatus.OK);
     }
 }
--- src/main/java/ua/agwebs/web/rest/Accounts/BalanceAccountService.java
+++ src/main/java/ua/agwebs/web/rest/Accounts/BalanceAccountService.java
@@ -4,4 +4,6 @@ package ua.agwebs.web.rest.Accounts;
 public interface BalanceAccountService {
 
     public BalanceAccountDTO findById(long bookId, long accountId);
+
+    public void createBalanceAccount(BalanceAccountDTO dto);
 }
