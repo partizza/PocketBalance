8b75bf0d62227e989e694d00a2d9bd56c6b7218b
Anton Gabrylevych <agabrylevych@gmail.com>
2017-04-10 21:53:00 +0300

    show user name

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -1,20 +1,32 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
 
-  $(".submenu > a").click(function(e) {
-    e.preventDefault();
-    var $li = $(this).parent("li");
-    var $ul = $(this).next("ul");
+    $(".submenu > a").click(function (e) {
+        e.preventDefault();
+        var $li = $(this).parent("li");
+        var $ul = $(this).next("ul");
 
-    if($li.hasClass("open")) {
-      $ul.slideUp(350);
-      $li.removeClass("open");
-    } else {
-      $(".nav > li > ul").slideUp(350);
-      $(".nav > li").removeClass("open");
-      $ul.slideDown(350);
-      $li.addClass("open");
-    }
-  });
-  
+        if ($li.hasClass("open")) {
+            $ul.slideUp(350);
+            $li.removeClass("open");
+        } else {
+            $(".nav > li > ul").slideUp(350);
+            $(".nav > li").removeClass("open");
+            $ul.slideDown(350);
+            $li.addClass("open");
+        }
+    });
+
+    $.ajax({
+        type: 'GET',
+        url: '../data/user/details',
+        dataType: 'json',
+        success: function (data) {
+
+            $('.dropdown-toggle').text(data.name + ' ' + data.surname);
+            $('.dropdown-toggle').append('<b class="caret"></b>');
+
+        }
+    });
+
 });
