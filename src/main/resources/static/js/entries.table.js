2d12d1bf33eec6acdb715e5cc7e7cd3c607ae11e
Anton Gabrylevych <agabrylevych@gmail.com>
2017-09-24 11:28:15 +0300

    HTML pages view imprv

--- src/main/resources/static/js/entries.table.js
+++ src/main/resources/static/js/entries.table.js
@@ -1,13 +1,7 @@
 $(document).ready(function () {
-    // $('.table.entries').DataTable( {
-    //     processing: true,
-    //     serverSide: true,
-    //     ajax: {
-    //         url: '/data/entry'
-    //     }
-    // } );
 
     $('.table.entries').DataTable({
+        searching: false,
         processing: true,
         serverSide: true,
         ajax: {
@@ -15,13 +9,37 @@ $(document).ready(function () {
         },
         columns: [
             {title: "Date", name: "header.valueDate", data: "headerValueDate"},
-            {title: "Category", name:"account.bsCategory", data: "accountBsCategory"},
-            {title: "accId", name:"account.accId", data: "accountAccId", visible: false},
-            {title: "Account", name:"account.name", data: "accountName"},
-            {title: "Comment", name:"header.desc", data: "headerDesc"},
-            {title: "curId", name:"currency.id", data: "currencyId", visible: false},
-            {title: "Currency", name:"currency.code", data: "currencyCode"},
-            {title: "Amount", name:"trnAmount", data: "trnAmount"}
+            {
+                title: "Category",
+                name: "account.bsCategory",
+                data: "accountBsCategory",
+                render: function (data, type, row) {
+                    return getAccountCategoryText(data);
+                }
+            },
+            {title: "accId", name: "account.accId", data: "accountAccId", visible: false},
+            {title: "Account", name: "account.name", data: "accountName"},
+            {
+                title: "Amount",
+                name: "trnAmount",
+                data: "trnAmount",
+                render: function (data, type, row) {
+                    return accounting.formatMoney(Number(data), {
+                        symbol: "",
+                        precision: 2,
+                        format: {
+                            pos: "%s %v",
+                            neg: "%s (%v)",
+                            zero: "%s  --"
+                        }
+                    });
+                }
+            },
+            {title: "curId", name: "currency.id", data: "currencyId", visible: false},
+            {title: "Currency", name: "currency.code", data: "currencyCode"},
+            {title: "Comment", name: "header.desc", data: "headerDesc"}
         ]
-    });
+    })
+    ;
+
 });
